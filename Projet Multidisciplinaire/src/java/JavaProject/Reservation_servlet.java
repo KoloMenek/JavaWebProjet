be14e552705681a5a9619efd31c651ff2a5b4658
KoloMenek <KoloMenek@DESKTOP-2IH87FJ>
2020-02-05 01:06:53 +0100

    avancé reservation

--- Projet Multidisciplinaire/src/java/JavaProject/Reservation_servlet.java
+++ Projet Multidisciplinaire/src/java/JavaProject/Reservation_servlet.java
@@ -16,6 +16,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import javax.servlet.http.Part;
 
 /**
@@ -24,35 +25,22 @@ import javax.servlet.http.Part;
  */
 @WebServlet(name = "Reservation_servlet", urlPatterns = {"/Reservation_servlet"})
 public class Reservation_servlet extends HttpServlet {
+    String URL = "/PageApresCreationCompte.jsp";
     
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        String[] checkboxes = request.getParameterValues("Choix");
-        int T[] = new int[checkboxes.length+1]; //Colonne
-        int T1[] = new int[checkboxes.length+1]; // Numéro
-        char c;
-        for (int i = 0; i < checkboxes.length; ++i) {   
-            if (checkboxes[i] != null){
-                c = checkboxes[i].charAt(0);
-                T[i] = c - '0';
-                if (checkboxes[i].length() > 2){
-                   String str = checkboxes[i].substring(1,3);
-                   T1[i] = Integer.parseInt(str);
-                }
-                else {
-                    c = checkboxes[i].charAt(1);
-                    T1[i] = c - '0';
-                }
-            }
-        }    
-        for (int i = 0; i<5;i++){
-            for (int j = 0; j<10; j++){
-                //T[i][j] = request.getParameter("");
-            }
-        }
+        HttpSession session = request.getSession();
+        Integer idClient = (Integer) session.getAttribute("idClient");
+        
+        String[] lesChoix = (String[]) request.getParameterValues("Choix");
+        for(int i =0;i<lesChoix.length;i++)
+            System.out.println(lesChoix[i]);
+        
+        
+        
             //request.setAttribute("map", T);
-            RequestDispatcher distributeur = request.getRequestDispatcher("/Reservation2.jsp");
+            RequestDispatcher distributeur = request.getRequestDispatcher(URL);
             distributeur.forward(request, response);
 
         }
